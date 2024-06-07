3e90d9df1c40d2383d7bfc26e4a9a197346d9219
Hayden Buckley-Smith <149153154+HaydenBSmith93@users.noreply.github.com>
2024-06-07 15:44:47 +0100

    chore: add color tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "calorie-main": "#2596be",
+        "calorie-orange": "#fdc472",
+        "calorie-salmon": "#f98072",
+        "calorie-pink": "#fcc2bb",
+        "calorie-navy": "#326aa1",
+      },
+    },
   },
 
   plugins: [],
